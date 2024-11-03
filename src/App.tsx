c8ea48d82eb258f6fc9aee60214b109bcdfcfded
Benboudina Moussa <121502039+MoussaBenboudina@users.noreply.github.com>
2024-11-03 12:37:01 -0800

    fix vite.confing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import InfiniteScroll from "./components/infiniteScroll";
 
 function App() {
   return (
-    <div className="w-full  bg-blue-500 p-4 lg:p-10 xl:p-20 ">
+    <div className="w-full  bg-blue-500 p-4 lg:p-10 xl:p-20 min-h-screen">
       <InfiniteScroll />
     </div>
   );
